7333569fbb94d08b52e67d48e4764887e5cc6f9d
Vibhu Sharma <109555986+vibhusharma2@users.noreply.github.com>
2024-02-15 12:05:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Leaders in an array/leaders-in-an-array.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter ot = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+
+		    for(int i=0; i<res.size(); i++){
+		        ot.print(res.get(i)+" ");
+		    }
+		    
+		    ot.println();
+		}
+		ot.close();
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+  public static int check(int arr[], int num, int idx) {
+    int res = num;
+    for (int i = idx; i < arr.length; i++) {
+      if (num < arr[i]) {
+        res = -1;
+        break;
+      }
+
+    }
+
+    return res;
+
+  }
+
+  static ArrayList < Integer > leaders(int arr[], int n) {
+
+    ArrayList < Integer > sp = new ArrayList < > ();
+
+    for (int i = 0; i < n; i++) {
+      if (check(arr, arr[i], i + 1) != -1) {
+        sp.add(arr[i]);
+      }
+
+    }
+
+    return sp;
+
+  }
+}
