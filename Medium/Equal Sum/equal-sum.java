8c686db7625e1a106a3fabc14c8bc4c16ee07f49
Vibhu Sharma <109555986+vibhusharma2@users.noreply.github.com>
2024-03-26 13:03:45 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Equal Sum/equal-sum.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            String ans = new Solution().equilibrium(a, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+     String equilibrium(int arr[], int n) {
+    // code here
+
+    int sum = 0;
+    
+    for (int i = 0; i < n; i++) {
+      sum += arr[i];
+    }
+    
+    int sum1 = 0;
+    
+    for (int i = 0; i < n; i++) {
+      if (i != 0) sum1 += arr[i - 1];
+      sum -= arr[i];
+      
+      if (sum1 == sum) return "YES";
+    }
+    
+    return "NO";
+  }
+}
